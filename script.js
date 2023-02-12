1dabd8b281d70eee73e449a66a68a5cffc3dada7
julio <iragijulien@gmail.com>
2023-02-12 22:53:28 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+/*function myFunction(){
+    var list = document.getElementById("list");
+    if(list.style.display === "flex"){
+        list.style.display = "none";
+    }else{
+        list.style.display = "flex";
+    }
+}*/
